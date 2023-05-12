45f67a2d3b2bfca22ee7abccc9f15dee0d0b6560
Hausel Kinga <hausel.kinga@gmail.com>
2023-05-12 09:23:29 +0200

    create /api/users register endpoint with Hello World

---
+++ GreenBay-Backend/GreenBay-Backend/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿namespace GreenBay_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
--- GreenBay-Backend/GreenBay-Backend/GlobalUsing.cs
+++ GreenBay-Backend/GreenBay-Backend/GlobalUsing.cs
@@ -1,3 +1,6 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using GreenBay_Backend.Models;
 global using GreenBay_Backend.Data;
+global using Microsoft.AspNetCore.Mvc;
+global using System.ComponentModel.DataAnnotations;
+global using GreenBay_Backend.DTO.Incoming;
