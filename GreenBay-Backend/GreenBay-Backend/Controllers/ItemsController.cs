4a443a3e2f44fd61794a11302d2336cb6a694cb1
Hausel Kinga <hausel.kinga@gmail.com>
2023-05-17 10:03:25 +0200

    add GetItemById endpoint to ItemsController

--- GreenBay-Backend/GreenBay-Backend/Controllers/ItemsController.cs
+++ GreenBay-Backend/GreenBay-Backend/Controllers/ItemsController.cs
@@ -22,5 +22,29 @@
 
             return Ok(result);
         }
+
+        [HttpGet("{id}", Name="GetItemById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<ItemDTO> GetItemById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            
+            var item = _itemRepository.GetItemById(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            var result = _mapper.Map<ItemDTO>(item);
+
+            return Ok(result);
+
+        }
     }
 }
