a8e79ac92906155dc90359229e8e4535c016657e
Hausel Kinga <hausel.kinga@gmail.com>
2023-05-17 12:02:49 +0200

    add ItemCreationDTO and implement mapping

---
+++ GreenBay-Backend/GreenBay-Backend/DTO/Incoming/ItemCreationDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace GreenBay_Backend.DTO.Incoming
+{
+    public class ItemCreationDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 128, MinimumLength = 3)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        [StringLength(maximumLength: 2048)]
+        public string PhotoURL { get; set; } = string.Empty;
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public string Seller { get; set; } = string.Empty;
+    }
+}
--- GreenBay-Backend/GreenBay-Backend/Profiles/ItemProfile.cs
+++ GreenBay-Backend/GreenBay-Backend/Profiles/ItemProfile.cs
@@ -29,6 +29,28 @@
                     dest => dest.Seller,
                     opt => opt.MapFrom(src => src.User.UserName)
                 );
+
+            CreateMap<ItemCreationDTO, Item>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => src.Name)
+                )
+                .ForMember(
+                    dest => dest.Description,
+                    opt => opt.MapFrom(src => src.Description)
+                )
+                .ForMember(
+                    dest => dest.PhotoURL,
+                    opt => opt.MapFrom(src => src.PhotoURL)
+                )
+                .ForMember(
+                    dest => dest.Price,
+                    opt => opt.MapFrom(src => src.Price)
+                )
+                .ForMember(
+                    dest => dest.User.UserName,
+                    opt => opt.MapFrom(src => src.Seller)
+                );
         }
     }
 }
