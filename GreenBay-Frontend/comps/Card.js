bc02625bbd219c06cf4f44fa234319f3659144ea
Hausel Kinga <hausel.kinga@gmail.com>
2023-05-23 08:42:08 +0200

    fix overflowing image in Card component

--- GreenBay-Frontend/comps/Card.js
+++ GreenBay-Frontend/comps/Card.js
@@ -4,7 +4,9 @@ import style from "../styles/Card.module.css";
 export default function Card({ item }) {
   return (
     <Link href={`/items/${item.id}`} className={`${style.container} center`}>
-      <img src={item.photoURL} />
+      <div className={style.imgContainer}>
+        <img src={item.photoURL} />
+      </div>
       <p>{item.name}</p>
       <p>{item.price} GBD</p>
     </Link>
